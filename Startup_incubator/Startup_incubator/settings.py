b4013baf99d0abe43b8c41ec02c5f63720b94280
Mayank <mayankchaurasia.bsp@gmail.com>
2019-02-14 02:34:28 +0530

    templates working

--- Startup_incubator/Startup_incubator/settings.py
+++ Startup_incubator/Startup_incubator/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Startup_incubator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Startup_incubator/startup/views.py
+++ Startup_incubator/startup/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
+	return render(request,"startup/register.html")
 	return HttpResponse('hello')
