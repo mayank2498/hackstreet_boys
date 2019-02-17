486f91fbb9f833ec3d45d97b602f90e88279a394
Mayank <mayankchaurasia.bsp@gmail.com>
2019-02-17 12:57:44 +0530

    recommend

--- Startup_incubator/recommendations/test.py
+++ Startup_incubator/recommendations/test.py
@@ -1,6 +1,6 @@
 from gensim.models import Word2Vec
 import numpy 
-
+from . import preprocess
 
 def avg_sentence_vector(words, model):
     num_features = 150
@@ -20,9 +20,24 @@ def avg_sentence_vector(words, model):
 
 
 def predict(investors,startup):
+   
+    startup = preprocess.normalize([startup])[0]
+    
     model = Word2Vec.load("word2vec.model")
     f1 = avg_sentence_vector(startup.split(),model)
+
+    data = []
     for investor in investors:
-        f2 = avg_sentence_vector(investor[1].split(),model)
-        cosine_similarity = numpy.dot(f1,f2)/(numpy.linalg.norm(f1)* numpy.linalg.norm(f2))
-        print(investor[0]," : ",cosine_similarity)
+        try:
+            investor[1] = preprocess.normalize([investor[1]])[0]
+         
+            f2 = avg_sentence_vector(investor[1].split(),model)
+            cosine_similarity = numpy.dot(f1,f2)/(numpy.linalg.norm(f1)* numpy.linalg.norm(f2))
+            temp = [investor[0],cosine_similarity]
+            data.append(temp)
+            print(investor[0]," : ",cosine_similarity)
+        except:
+            temp = [investor[0],-100]
+            data.append(temp)
+    return data
+
